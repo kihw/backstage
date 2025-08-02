3d29807a2236b93b5c5764a2f7be4b6b5bc701e8
kihw <kihwouih@gmail.com>
2025-08-02 16:52:16 +0200

    Remove unused Button import for cleaner code

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 import {
   EntityHasComponentsCard,
   EntityHasResourcesCard,
@@ -44,6 +43,7 @@ import {
   HeaderTabs,
   InfoCard,
   Page,
+  Button,
 } from '@backstage/core-components';
 
 const entityWarningContent = (
